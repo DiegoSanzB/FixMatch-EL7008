Fixed impurity and mask_rate calculation per epoch to float, reduced size of labeled data

main.py
@@ -2,7 +2,7 @@ import logging
 from data.dataset import get_cifar10, CIFAR10
 from models.vgg import VGG16, VGG9
 from models.myNet import MyNet
-from utils import accuracy_confusion, plot_loss, prediction
+from utils import accuracy_confusion, plot_loss, plot_impurity_mask_rate, prediction
 from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
@@ -23,29 +23,29 @@ LOADER_LENGHT = 50  # Same for supervised and unsupervised loaders
 
 if __name__ == '__main__':
     logger.info(f'Using Device: {DEVICE}')
-    # setup dataset and dataloaders
+    # obtain data
     train_data, train_labels, test_data, test_labels = get_cifar10()
     train_data, val_data, train_labels, val_labels = train_test_split(
         train_data, train_labels,
-        train_size=0.8, random_state=SEED, shuffle=SHUFFLE
+        train_size=0.95, random_state=SEED, shuffle=SHUFFLE
     )
     supervised_data, unsupervised_data, supervised_labels, unsupervised_labels = train_test_split(
         train_data, train_labels,
-        train_size=0.2, random_state=SEED, shuffle=SHUFFLE
+        train_size=0.1, random_state=SEED, shuffle=SHUFFLE
     )
-
+    # setup datasets
     Cifar10_supervised = CIFAR10(supervised_data, supervised_labels)
     Cifar10_unsupervised = CIFAR10(unsupervised_data, unsupervised_labels)
     Cifar10_val = CIFAR10(val_data, val_labels)
     Cifar10_test = CIFAR10(test_data, test_labels)
 
-    logger.info(f'Size of supervised dataset {len(Cifar10_supervised)} and unsupervised {len(Cifar10_unsupervised)}')
+    logger.info(f'Size of supervised dataset {len(Cifar10_supervised)}, unsupervised {len(Cifar10_unsupervised)} and validation {len(Cifar10_val)}')
 
     supervised_batch_size = int(np.ceil(len(Cifar10_supervised) / LOADER_LENGHT))
     unsupervised_batch_size = int(np.ceil(len(Cifar10_unsupervised) / LOADER_LENGHT))
 
     logger.info(f' with supervised batches of size {supervised_batch_size} and unsupervised {unsupervised_batch_size}')
-
+    # setup dataloaders
     supervised_loader = DataLoader(Cifar10_supervised, batch_size=supervised_batch_size, 
                             shuffle=SHUFFLE, num_workers=2, pin_memory=True)
     unsupervised_loader = DataLoader(Cifar10_unsupervised, batch_size=unsupervised_batch_size, 
@@ -61,14 +61,23 @@ if __name__ == '__main__':
     net = MyNet()
     net.to(DEVICE)
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(net.parameters(), lr=3e-4)
-
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-4)
+    # training
     supervised_loss, unsupervised_loss, val_loss, impurity, mask_rate = train_fixmatch(
         net, supervised_loader, unsupervised_loader, val_loader, optimizer,
         criterion, CHECKPOINT_PATH
     )
+    train_metrics = np.vstack((supervised_loss, unsupervised_loss, val_loss, impurity, mask_rate))
+    
+    with open('data.npy', 'wb') as f:
+        np.save(f, train_metrics)
+
+    # obtain metrics
     labels_true, labels_pred = prediction(net, test_loader)
-    accuracy = accuracy_confusion(labels_true, labels_pred, 'Fixmatch Confusion Matrix')
+    accuracy = accuracy_confusion(labels_true, labels_pred, 'Fixmatch Confusion Matrix', 'fixmatch_cm.png')
 
-    plot_loss((supervised_loss, unsupervised_loss, val_loss), ('Supervised', 'Unsupervised', 'Validation'))
+    plot_loss((supervised_loss, unsupervised_loss, val_loss), ('Supervised', 'Unsupervised', 'Validation'), 'fixmatch_all_loss.png')
 
+    plot_loss((supervised_loss + unsupervised_loss, val_loss), ('Train', 'Validation'), 'fixmatch_loss.png')
+
+    plot_impurity_mask_rate(impurity, mask_rate, 'fixmatch_impurity_mask_rate.png')

train.py
@@ -5,8 +5,8 @@ import numpy as np
 from data.augmentation import RandAugment, SoftAugment
 from utils import DEVICE
 
-EPOCHS = 100
-PATIENCE = 10
+EPOCHS = 50
+PATIENCE = 5
 N, M = 2, 10
 
 logger = logging.getLogger(__name__)
@@ -48,7 +48,7 @@ def train_fixmatch(model, supervised_loader, unsupervised_loader, val_loader, op
             inputs_u, labels_u = next(iter_unsupervised)
             inputs_u = inputs_u.float()
             labels_u = labels_u.to(DEVICE)
-            # we don't use the real unsupervised labels during training
+            # we don't use the real unsupervised labels during training, only for impurity
 
             # compute supervised loss   
             optimizer.zero_grad()
@@ -76,15 +76,18 @@ def train_fixmatch(model, supervised_loader, unsupervised_loader, val_loader, op
             # Process soft augmented data
             soft_output = model(soft_tensor)
             soft_output = softmax(soft_output)
+            
             # Create pseudo labels
             soft_max, soft_argmax = torch.max(soft_output, dim=1)
             soft_threshold = soft_max.ge(tau).float().to(DEVICE)
+            
             # compute impurity and mask rate
             surpassed_threshold += soft_threshold.sum()
-            incorrect_predictions += (soft_argmax.ne(labels_u)) * soft_threshold
+            incorrect_predictions += ((soft_argmax.ne(labels_u)) * soft_threshold).sum()
             
             # Process strong augmented data
             strong_output = model(strong_tensor)
+            
             # Get loss from strong augmentation given the pseudo labels
             unsupervised_loss = criterion(strong_output, soft_argmax) * soft_threshold
             unsupervised_loss = unsupervised_loss.mean()     
@@ -99,8 +102,8 @@ def train_fixmatch(model, supervised_loader, unsupervised_loader, val_loader, op
         supervised_losses.append(epoch_loss_supervised/len(iter_supervised))
         unsupervised_losses.append(epoch_loss_unsupervised/len(iter_supervised))
 
-        impurity.append(incorrect_predictions / surpassed_threshold)
-        mask_rate.append(surpassed_threshold / len(iter_unsupervised))
+        impurity.append((incorrect_predictions / surpassed_threshold).item())
+        mask_rate.append((surpassed_threshold / len(iter_unsupervised)).item())
 
         # Validation with val_loader
         model.eval()
@@ -140,11 +143,11 @@ def train_fixmatch(model, supervised_loader, unsupervised_loader, val_loader, op
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     epoch_cp = checkpoint['epoch']
-    supervised_loss_cp = checkpoint['supervised_loss']
-    unsupervised_loss_cp = checkpoint['unsupervised_loss']
-    val_loss_cp = checkpoint['val_loss']
+    #supervised_loss_cp = checkpoint['supervised_loss']
+    #unsupervised_loss_cp = checkpoint['unsupervised_loss']
+    #val_loss_cp = checkpoint['val_loss']
 
-    logger.info(f' Loaded model from epoch {epoch_cp} with supervised loss {supervised_loss_cp: .5f}, unsupervised loss {unsupervised_loss_cp: .5f} and val loss {val_loss_cp: .5f} ')
+    logger.info(f' Loaded model from epoch {epoch_cp}') # with supervised loss {supervised_loss_cp: .5f}, unsupervised loss {unsupervised_loss_cp: .5f} and val loss {val_loss_cp: .5f} 
 
     return np.array(supervised_losses), np.array(unsupervised_losses), np.array(val_loss), np.array(impurity), np.array(mask_rate)
 

utils.py
@@ -6,18 +6,20 @@ import torch
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def accuracy_confusion(y_true, y_pred, title):
+def accuracy_confusion(y_true, y_pred, title, path):
     accuracy = accuracy_score(y_true, y_pred)
     conf_matrix = confusion_matrix(y_true, y_pred, normalize='true')
 
     s = sns.heatmap(conf_matrix, linewidths=.1, annot=True, square=True)
     s.set(xlabel='Predicted', ylabel='GT', title=f'{title}, accuracy = {accuracy}')
+    
+    plt.savefig(path)
     plt.show()
 
     return accuracy
 
 
-def plot_loss(losses, legends):
+def plot_loss(losses, legends, path):
     for loss, legend in zip(losses, legends):
         x = np.arange(loss.shape[0])
         plt.plot(x, loss, label=legend)
@@ -25,7 +27,27 @@ def plot_loss(losses, legends):
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
+    plt.savefig(path)
     plt.show()
+
+
+def plot_impurity_mask_rate(impurity, mask_rate, path):
+    f, axis = plt.subplots(1, 2)
+    x = np.arange(impurity.shape[0])
+
+    axis[0].plot(x, impurity)
+    axis[0].set_title('Impurity')
+
+    axis[1].plot(x, mask_rate)
+    axis[1].set_title('Mask Rate')
+
+    for ax in axis.flat:
+        ax.set(xlabel='Epoch')
+
+    plt.savefig(path)
+    plt.show()
+    
+    
 
 
 def prediction(model, dataloader):